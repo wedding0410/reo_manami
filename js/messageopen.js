f9c1947fea19eaf348a788bb35cee7b7b4bbed09
wedding0410 <miharu0428.gm@gmail.com>
2022-03-09 01:16:03 +0900

    d

---
+++ js/messageopen.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.item-demo').on('click',function(){
+        $('.popup-demo').addClass('popup-show').fadeIn();
+    });
+    $('.close').on('click',function(){
+        $('.popup-demo').fadeOut();
+    });
+    });
+     
