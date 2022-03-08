ee31e8b6bc4d866def2ca882b7c68e5f9f65cf00
wedding0410 <miharu0428.gm@gmail.com>
2022-03-09 00:27:23 +0900

    f

--- js/bg.js
+++ js/bg.js
@@ -1,6 +1,6 @@
 bubbly({
-    colorStart: '#fcf7f2', //背景色左上
-    colorStop: '#fcf9f5',　//背景色右下
+    colorStart: '#000066', //背景色左上
+    colorStop: '#aaaaaa',//背景色右下
     blur:1,
     compose: 'source-over',
     bubbleFunc:() => `hsla(${Math.random() * 50}, 20%, 50%, .3)`
