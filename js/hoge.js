137a2eebb00eaa629fd8b1f4c3132be506fb36f2
wedding0410 <miharu0428.gm@gmail.com>
2022-03-25 00:53:24 +0900

    d

---
+++ js/hoge.js
@@ -0,0 +1,22 @@
+$(function() {
+	setTimeout(function(){
+		$('.start p').fadeIn(1600);
+	},200); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start p1').fadeIn(1600);
+	},500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start p2').fadeIn(1600);
+	},1500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start p3').fadeIn(1600);
+	},2500); //0.5秒後にロゴをフェードイン!
+	setTimeout(function(){
+		$('.start').fadeOut(500);
+	},4500); //2.5秒後にロゴ含め真っ白背景をフェードアウト！
+
+
+	
+});
+
+
